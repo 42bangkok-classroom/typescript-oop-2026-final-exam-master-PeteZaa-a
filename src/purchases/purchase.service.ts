ab7cbb9bc4ffa7941c9d0f31fd5c99d2c5500362
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-03-25 10:37:50 +0700

    6

--- src/purchases/purchase.service.ts
+++ src/purchases/purchase.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from "@nestjs/common";
-import * as fs from "fs";
-import { join } from "path";
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class PurchaseService {
-    private readonly dbPathPurchase = join(process.cwd(), 'data/purchase.json')
+  private readonly dbPathPurchase = join(process.cwd(), 'data/purchase.json');
 
-    findAll() {
-        const readDb = fs.readFileSync(this.dbPathPurchase, 'utf8')
-        return JSON.parse(readDb) as string
-    }
+  findAll() {
+    const readDb = fs.readFileSync(this.dbPathPurchase, 'utf8');
+    return JSON.parse(readDb) as string;
+  }
 }
