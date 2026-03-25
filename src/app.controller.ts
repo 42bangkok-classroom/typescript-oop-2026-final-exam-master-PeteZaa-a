afe05f2f591ca495db6dc8658622728f8d629bfd
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-03-25 09:48:27 +0700

    1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { ApiResponse } from './interfaces/response.interface';
 
-@Controller()
+@Controller('products')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): ApiResponse<T> {
+    const result = this.appService.getHello();
+    return {
+      "success": true,
+      "data": {
+        "service": 'purchase-api',
+        "version": '1.0.0'
+      },
+      "message": 'Hello NestJS'
+    }
+  }
+  @Get()
+  findAll(): ApiResponse<T> {
+    const result = this.appService.findAll();
+    return {
+      "success": true,
+      "data": result,
+      "message": 'Fetched products successfully'
+    }
+
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import type { ApiResponse } from './interfaces/response.interface';
+import { join } from 'path';
+import * as fs from 'fs'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): ApiResponse<T> {
+    return 'Hello NestJS';
+  }
+  
+  private readonly dbProduct = join(process.cwd(), 'data/product.json')
+  findAll() {
+    const readDb = fs.readFileSync(this.dbProduct, 'utf8')
+    return JSON.parse(readDb)
   }
 }
